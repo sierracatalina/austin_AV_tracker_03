0577af272c049797c34aab9b11f8b6e32ce7e64a
sierracatalina <sierracatalina1@gmail.com>
2025-06-26 15:53:36 -0400

    Add Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      animation: {
+        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+      colors: {
+        'red-400': '#f87171',
+        'red-500': '#ef4444',
+        'orange-500': '#f97316',
+        'gray-900': '#111827',
+        'gray-800': '#1f2937',
+        'gray-700': '#374151',
+        'gray-600': '#4b5563',
+        'gray-500': '#6b7280',
+        'gray-400': '#9ca3af',
+        'gray-300': '#d1d5db',
+      }
+    },
+  },
+  plugins: [],
+}
